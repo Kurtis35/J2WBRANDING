be72305b5ab8a3f59a25bb1e6b1c9cea3252e57f
Kurtis35 <kriebodini@gmail.com>
2025-03-13 19:54:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Smooth scrolling for anchor links
     $('a[href*="#"]').not('[href="#"]').click(function(event) {
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
